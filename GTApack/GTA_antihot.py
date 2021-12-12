74ba1231fc3d303867a68b6eb6fcfc17a828d31c
Javert-ctrl <79468558+Javert-ctrl@users.noreply.github.com>
2021-12-12 14:02:28 +0100

    Setup of training + using the models from neptune

--- GTApack/GTA_antihot.py
+++ GTApack/GTA_antihot.py
@@ -10,7 +10,7 @@ def GTA_antihot(onehot_img, colors, width, height):
 
 
     map = torch.empty((3, height, width), dtype=torch.uint8)
-    for i in range(0, len(colors)):
+    for i in range(0, len(set(colors.values()))):
         col = inv_colors.get(i)
         indicies = np.where(mat[i] == 1)
         for r, c in zip(*indicies):
